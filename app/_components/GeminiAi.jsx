eccd9b2df9069765e99c11949e76f5d55f9284c3
Premlakshay Godara <89251510+Premlak@users.noreply.github.com>
2025-01-23 07:42:06 +0530

    Update GeminiAi.jsx

--- app/_components/GeminiAi.jsx
+++ app/_components/GeminiAi.jsx
@@ -4,7 +4,7 @@ const {
     HarmBlockThreshold,
   } = require("@google/generative-ai");
   
-  const apiKey = process.env.NEXT_PUBLIC_GEMINI_API_KEY;
+  const apiKey = "AIzaSyCM4SMgtUjkELjcfcMdlkto9L0c-v-k4Po";
   const genAI = new GoogleGenerativeAI(apiKey);
   
   const model = genAI.getGenerativeModel({
